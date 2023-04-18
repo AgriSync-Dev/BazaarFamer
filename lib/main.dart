ae1aafc1f8a10bf4ff419192192de35ef279fc9b
Amartya Yadav <iamartyayadav@gmail.com>
2023-04-18 14:11:37 +0530

    login sign up

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,52 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:pirate_hunt/screens/login_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:pirate_hunt/screens/signup_screen.dart';
 
-void main() {
+import 'screens/drop_screen.dart';
+import 'screens/home_screen.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Farmer Market',
+      title: 'Bazaar',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
+      ), 
+      
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, userSnapshot) {
+          if (userSnapshot.connectionState == ConnectionState.waiting) {
+            return const DropScreen();
+          }
+
+          if (userSnapshot.hasData) {
+            return const HomeScreen();
+          }
+          else {
+            return const LoginScreen();
+          }
+        },
       ),
-      home: const Text('Home Page'),
+      routes: {
+        LoginScreen.routeName: (context) => const LoginScreen(),
+        SignUpScreen.routeName:(context) => const SignUpScreen(),
+        HomeScreen.routeName: (context) => const HomeScreen(),
+        DropScreen.routeName: (context) => const DropScreen(),
+      },
     );
   }
 }
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,58 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:pirate_hunt/model/user_model.dart';
+import 'package:pirate_hunt/screens/login_screen.dart';
+
+class HomeScreen extends StatefulWidget {
+  static const routeName = '/home-screen';
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  User? user = FirebaseAuth.instance.currentUser;
+  UserModel loggedInUser = UserModel();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    FirebaseFirestore.instance
+        .collection("users")
+        .doc(user!.uid)
+        .get()
+        .then((value) {
+      loggedInUser = UserModel.fromMap(value.data());
+      setState(() {});
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Bazaar'),
+        centerTitle: true,
+        leading: IconButton(onPressed: (){
+          logout(context);
+        }, icon: const Icon(Icons.arrow_back)),
+      ),
+      body: const Center(
+        child: Text('Home'),
+      ),
+    );
+  }
+
+  Future<void> logout(BuildContext context) async {
+    await FirebaseAuth.instance.signOut();
+    Navigator.of(context).pushReplacement(
+      MaterialPageRoute(
+        builder: (ctx) => const LoginScreen(),
+      ),
+    );
+  }
+}
