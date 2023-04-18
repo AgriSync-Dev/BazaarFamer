b6a650f9e90811012c6f5ca2c902f4d6c6f33f79
apexpre-dator <96236927+apexpre-dator@users.noreply.github.com>
2023-04-18 17:20:43 +0530

    profile screen

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -11,7 +11,6 @@ import 'components/categories.dart';
 import 'components/new_arrival_products.dart';
 import 'components/popular_products.dart';
 import 'components/search_form.dart';
-import '/screens/cart/components/badge.dart';
 import 'components/section_title.dart';
 
 class SecondHomeScreen extends StatefulWidget {
@@ -57,7 +56,8 @@ class _SecondHomeScreenState extends State<SecondHomeScreen> {
           onPressed: () {
             logout(context);
           },
-          icon: SvgPicture.asset("assets/icons/menu.svg"),
+          icon: const Icon(Icons.logout),
+          //SvgPicture.asset("assets/icons/menu.svg"),
         ),
         title: Row(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -106,33 +106,35 @@ class _SecondHomeScreenState extends State<SecondHomeScreen> {
               ],
             ),
           ),
-          Center(child: Text('Orders')),
-          Center(child: Text('Profile'),),
+          Center(
+            child: Text('Orders'),
+          ),
+          Center(
+            child: Text('Profile'),
+          ),
         ],
       ),
       bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.blueAccent,
         items: [
           BottomNavigationBarItem(
             icon: Icon(
               Icons.home,
-              color: _page == 0 ? primaryColor : secondaryColor,
+              color: _page == 0 ? Colors.white : Colors.grey,
             ),
             label: '',
           ),
-          
           BottomNavigationBarItem(
             icon: Icon(
               Icons.shopping_bag,
-              color: _page == 2 ? primaryColor : secondaryColor,
+              color: _page == 1 ? Colors.white : Colors.grey,
             ),
             label: '',
           ),
-         
           BottomNavigationBarItem(
             icon: Icon(
               Icons.person,
-              color: _page == 4 ? primaryColor : secondaryColor,
+              color: _page == 2 ? Colors.white : Colors.grey,
             ),
             label: '',
           ),
