27c151219165bbc746ea4e8191db1886544504a5
apexpre-dator <96236927+apexpre-dator@users.noreply.github.com>
2023-04-19 07:02:07 +0530

    add product changes

--- lib/screens/myproducts/add_product.dart
+++ lib/screens/myproducts/add_product.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:pirate_hunt/models/Product.dart';
 
@@ -9,58 +11,144 @@ class AddProduct extends StatefulWidget {
 }
 
 class _AddProductState extends State<AddProduct> {
-  List<Product> temp = [];
-  TextEditingController _title = TextEditingController();
-  TextEditingController _price = TextEditingController();
+  late List<Product> temp;
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _title = TextEditingController();
+  final TextEditingController _price = TextEditingController();
+  final TextEditingController _description = TextEditingController();
+
+  @override
+  void initState() {
+    temp = [];
+    super.initState();
+  }
 
   @override
   void dispose() {
     super.dispose();
     _title.dispose();
     _price.dispose();
+    _description.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
+    final nameField = TextFormField(
+      autofocus: false,
+      controller: _title,
+      validator: (value) {
+        if (value!.isEmpty) {
+          return ("Please enter your name!");
+        }
+        return null;
+      },
+      keyboardType: TextInputType.name,
+      onSaved: (val) {
+        _title.text = val!;
+      },
+      textInputAction: TextInputAction.next,
+      decoration: InputDecoration(
+        prefixIcon: const Icon(Icons.person),
+        contentPadding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
+        hintText: "Name",
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+      ),
+    );
+
+    final descriptionField = TextFormField(
+      maxLines: 5,
+      minLines: 5,
+      autofocus: false,
+      controller: _description,
+      validator: (value) {
+        if (value!.isEmpty) {
+          return ("Please enter the description!");
+        }
+        return null;
+      },
+      keyboardType: TextInputType.text,
+      onSaved: (val) {
+        _description.text = val!;
+      },
+      textInputAction: TextInputAction.next,
+      decoration: InputDecoration(
+        prefixIcon: const Icon(Icons.description),
+        contentPadding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
+        hintText: "Product Description",
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+      ),
+    );
+    final priceField = TextFormField(
+      autofocus: false,
+      controller: _price,
+      validator: (value) {
+        if (value!.isEmpty) {
+          return ("Please enter price of product!");
+        }
+        return null;
+      },
+      keyboardType: TextInputType.number,
+      onSaved: (val) {
+        _price.text = val!;
+      },
+      textInputAction: TextInputAction.next,
+      decoration: InputDecoration(
+        prefixIcon: const Icon(Icons.attach_money),
+        contentPadding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
+        hintText: "Price",
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+      ),
+    );
+
     return Scaffold(
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              Text('Enter your Name'),
-              SizedBox(
-                height: 15,
-              ),
-              TextField(controller: _title),
-              SizedBox(
-                height: 15,
-              ),
-              Text('Enter the price of product'),
-              SizedBox(
-                height: 15,
-              ),
-              TextField(
-                controller: _price,
-                keyboardType: TextInputType.number,
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  temp.add(
-                    Product(
-                      id: '123',
-                      image: '1234',
-                      title: _title.text,
-                      price: int.parse(_price.text),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                nameField,
+                const SizedBox(
+                  height: 15,
+                ),
+                priceField,
+                const SizedBox(
+                  height: 15,
+                ),
+                descriptionField,
+                const SizedBox(
+                  height: 15,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    temp.add(
+                      Product(
+                        id: '123',
+                        image: '1234',
+                        title: _title.text,
+                        price: int.parse(_price.text),
+                        description: _description.text,
+                      ),
+                    );
+                    //push data to firebase .................................................
+                  },
+                  child: const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text(
+                      'Add Product',
+                      style: TextStyle(fontSize: 16),
                     ),
-                  );
-                },
-                child: Text('Add Product'),
-              ),
-            ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
